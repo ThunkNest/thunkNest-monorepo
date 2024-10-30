8a98b321a4c4c88c45ada5f161fddc6f3bb7a158
tolanny-dev <tol43454@gmail.com>
2024-10-30 13:00:35 -0500

    complete setup for initial 3 services

---
+++ common-library/src/main/java/com/validate/monorepo/commonlibrary/CommonLibraryMain.java
@@ -0,0 +1,11 @@
+package com.validate.monorepo.commonlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonLibraryMain {
+	public static void main(String[] args) {
+		SpringApplication.run(CommonLibraryMain.class, args);
+	}
+}
