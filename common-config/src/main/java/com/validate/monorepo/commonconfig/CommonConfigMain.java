aab3a5f8688f4b75bcdf14035e7e5371c22f2122
tolanny-dev <tol43454@gmail.com>
2024-10-30 14:46:36 -0500

    add common config and update project build.gradle

---
+++ common-config/src/main/java/com/validate/monorepo/commonconfig/CommonConfigMain.java
@@ -0,0 +1,11 @@
+package com.validate.monorepo.commonconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonConfigMain {
+	public static void main(String[] args) {
+		SpringApplication.run(CommonConfigMain.class, args);
+	}
+}
